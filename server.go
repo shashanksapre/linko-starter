16b935c16636ff98c1f55fa54201705cb44723a9
Shashank Sapre <shashank.sapre@outlook.com>
2026-06-20 21:53:50 +0530

    What Is Observability?

--- server.go
+++ server.go
@@ -47,13 +47,17 @@ func (s *server) start() error {
 	if err != nil {
 		return err
 	}
+
 	if err := s.httpServer.Serve(ln); !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
+
+	fmt.Printf("Linko is running on http://localhost:%d\n", ln.Addr().(*net.TCPAddr).Port)
 	return nil
 }
 
 func (s *server) shutdown(ctx context.Context) error {
+	fmt.Printf("Linko is shutting down\n")
 	return s.httpServer.Shutdown(ctx)
 }
 
