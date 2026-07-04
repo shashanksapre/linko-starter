9ced4a618da1c0d73490d868d24502760da58f58
Shashank Sapre <shashank.sapre@outlook.com>
2026-07-04 09:49:23 +0530

    Logger Cleanup

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"context"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -14,26 +15,50 @@ import (
 	"boot.dev/linko/internal/store"
 )
 
-func initializeLogger() *log.Logger {
+type closeFunc func() error
+
+func initializeLogger() (*log.Logger, closeFunc, error) {
 	logFileLocation := os.Getenv("LINKO_LOG_FILE")
 
 	var logger *log.Logger
 
 	if logFileLocation == "" {
 		logger = log.New(os.Stderr, "", log.LstdFlags)
+
+		noOp := func() error {
+			return nil
+		}
+
+		return logger, noOp, nil
 	} else {
 		logFile, err := os.OpenFile(logFileLocation, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
 
 		if err != nil {
-			log.Fatalf("failed to open log file: %v", err)
+			return nil, nil, fmt.Errorf("failed to open log file: %w", err)
 		}
 
 		bufferedFile := bufio.NewWriterSize(logFile, 8192)
 		multiWriter := io.MultiWriter(os.Stderr, bufferedFile)
 		logger = log.New(multiWriter, "", log.LstdFlags)
-	}
 
-	return logger
+		closer := func() error {
+			err := bufferedFile.Flush()
+
+			if err != nil {
+				return err
+			}
+
+			err = logFile.Close()
+
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		return logger, closer, nil
+	}
 }
 
 func main() {
@@ -49,15 +74,31 @@ func main() {
 }
 
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
-	logger := initializeLogger()
+	logger, closer, err := initializeLogger()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to initalise file logger: %v\n", err)
+		return 1
+	}
+
+	defer func() {
+		err := closer()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "some error: %v\n", err)
+		}
+	}()
+
 	st, err := store.New(dataDir, logger)
 
 	if err != nil {
 		logger.Printf("failed to create store: %v", err)
 		return 1
 	}
+
 	s := newServer(*st, httpPort, cancel, logger)
 	var serverErr error
+
 	go func() {
 		serverErr = s.start()
 	}()
@@ -70,6 +111,7 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 		logger.Printf("failed to shutdown server: %v", err)
 		return 1
 	}
+
 	if serverErr != nil {
 		logger.Printf("server error: %v", serverErr)
 		return 1
