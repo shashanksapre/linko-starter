09ef5d6c5fa6eb1d93b9e64be1cdeb5b245b0f09
Shashank Sapre <shashank.sapre@outlook.com>
2026-07-04 11:58:13 +0530

    Output Formats

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func initializeLogger() (*slog.Logger, closeFunc, error) {
 
 		bufferedFile := bufio.NewWriterSize(logFile, 8192)
 
-		infoHandler := slog.NewTextHandler(bufferedFile, &slog.HandlerOptions{
+		infoHandler := slog.NewJSONHandler(bufferedFile, &slog.HandlerOptions{
 			Level: slog.LevelInfo,
 		})
 
