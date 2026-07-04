ea5b8e67a53bcc6546eeddec5ef80c6bfe7c40e8
Shashank Sapre <shashank.sapre@outlook.com>
2026-07-04 12:45:26 +0530

    Redundant Logs

--- auth.go
+++ auth.go
@@ -34,7 +34,7 @@ func (s *server) authMiddleware(next http.Handler) http.Handler {
 		}
 		ok, err := s.validatePassword(password, stored)
 		if err != nil {
-			s.logger.Error("error validating password", slog.String("username", username), "error", err)
+			s.logger.Error("error validating password", slog.String("user", username), "error", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
@@ -53,7 +53,6 @@ func (s *server) validatePassword(password, stored string) (bool, error) {
 		return false, nil
 	}
 	if err != nil {
-		s.logger.Error("error validating password", "error", err)
 		return false, err
 	}
 	return true, nil
